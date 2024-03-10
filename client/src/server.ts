2d49460aacb55c3b7f6f71311f10a27790a9c974
Kaarti22 <kaartimondi@gmail.com>
2024-03-10 22:33:20 +0530

    setup payload

---
+++ client/src/server.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { getPayloadClient } from "./get-payload";
+import { nextApp, nextHandler } from "./next-utils";
+import payload from "payload";
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+const start = async () => {
+    const payload = await getPayloadClient({
+        initOptions: {
+            express: app,
+            onInit: async (cms) => {
+                cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+            },
+        },
+    });
+
+    app.use((req, res) => nextHandler(req, res));
+    
+    nextApp.prepare().then(() => {
+        // payload.logger.info("NextJS started");
+        app.listen(PORT, async () => {
+            // payload.logger.info(`NextJS App URL: ${process.env.NEXT_PUBLIC_SVER_URL}`);
+        })
+    });
+};
+
+start();
